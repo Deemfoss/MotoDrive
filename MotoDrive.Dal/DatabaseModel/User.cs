fb67c6ba039090f361a12dff7bc058a35f117e89
artem <demfoss@gmail.com>
2017-10-23 02:29:07 +0300

    Changed structure

---
+++ MotoDrive.Dal/DatabaseModel/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MotoDrive.Dal.DatabaseModel
+{
+   public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        [ForeignKey("Roles")]
+        public string RoleId { get; set; }
+        public Roles Roles { get; set; }
+    }
+
+    public class Roles
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Role { get; set; }
+    }
+
+}
---
+++ MotoDrive.Web/Startup.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MotoDrive.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
